ea98f35847187eb17c4139cadbd4eaa0bb2d59fc
Gabriele Westh Mannucci <gab.mannu@hotmail.it>
2020-03-16 23:57:11 +0100

    added delete with alert and moved recipe-item to its own component

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -41,4 +41,10 @@ export class RecipesService {
     };
   }
 
+  deleteRecipe(recipeId: string) {
+    this.recipes = this.recipes.filter(recipe => {
+      return recipe.id !== recipeId;
+    });
+  }
+
 }
