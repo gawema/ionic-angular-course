b9a0e3da44050170ab6a80e081bf42f609e542f6
Gabriele Westh Mannucci <gab.mannu@hotmail.it>
2020-03-16 16:03:42 +0100

    added clear function and total expenses span

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const amountInput = document.querySelector('#input-amount');
 const clearBtn = document.querySelector('#btn-clear');
 const addbtn = document.querySelector('#btn-add');
 const expensesList = document.querySelector('#expenses-list');
+const totalExpensesOutput = document.querySelector('#total-expenses');
+
+var totalExpenses = 0;
+
+const clear = () => {
+  reasonInput.value = '';
+  amountInput.value = '';
+}
 
 addbtn.addEventListener('click', ()=>{
   const enteredReason = reasonInput.value;
@@ -21,4 +29,14 @@ addbtn.addEventListener('click', ()=>{
 
   expensesList.appendChild(newItem);
 
+  //the '+' in front of enteredAmount serves to convert the string into a number
+  totalExpenses += +enteredAmount;
+
+  totalExpensesOutput.textContent = totalExpenses;
+
+  clear();
+
 })
+
+// notice that the clear method reference has to be without '()'
+clearBtn.addEventListener('click', clear)
